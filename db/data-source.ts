ce01ccc1aea52c8c966f42cf8f0cdf4f87066cb9
Abdoul <abdoulkarim972012@gmail.com>
2024-12-12 14:00:46 +0000

    task backend commit nstl

--- db/data-source.ts
+++ db/data-source.ts
@@ -9,6 +9,9 @@ export const dataSourceOptions:DataSourceOptions = {
       username:process.env.DB_USERNAME,
       password:process.env.DB_PASSWORD,
       database:process.env.DB_DATABASE,
+       ssl: {
+    rejectUnauthorized: false, // Utilisez `false` pour éviter les problèmes de certificat en local
+  },
       entities: ['dist/**/*.entity{.ts,.js}'],
       migrations:['dist/db/migrations/*{.ts,.js}'],
       logging:false,
@@ -17,3 +20,23 @@ export const dataSourceOptions:DataSourceOptions = {
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
+
+
+
+// import { DataSource } from 'typeorm';
+
+// const dataSource = new DataSource({
+//   type: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT, 10),
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   ssl: {
+//     rejectUnauthorized: false, // Utilisez `false` pour éviter les problèmes de certificat en local
+//   },
+//   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//   synchronize: true, // À désactiver en production !
+// });
+
+// export default dataSource;
