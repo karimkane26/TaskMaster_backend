98e1b1b2dc76a74374cdec059b4b2e4938d59943
Abdoul <abdoulkarim972012@gmail.com>
2024-12-14 23:43:00 +0000

    task backend commit capstone

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import *  as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.enableCors({
@@ -12,6 +13,7 @@ async function bootstrap() {
     credentials: true,  // Pour accepter les cookies si vous utilisez des cookies pour l'authentification
   });
   app.use(cookieParser());
+  app.use(cors());
   app.useGlobalPipes(new ValidationPipe());
     app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
