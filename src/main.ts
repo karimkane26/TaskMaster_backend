bdddb77c032e658c4cd8460fa34307861af8ecfe
Abdoul <abdoulkarim972012@gmail.com>
2024-12-16 13:10:22 +0000

    task backend commit capstone

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import *  as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.enableCors({
-    origin: 'http://localhost:3000',  // Spécifiez l'URL de votre frontend
-    methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE'],  // Liste des méthodes autorisées
-    allowedHeaders: ['Content-Type', 'Authorization'],  // En-têtes autorisés (ajoutez d'autres si nécessaire)
-    credentials: true,  // Pour accepter les cookies si vous utilisez des cookies pour l'authentification
+
+  // Configuration de CORS avec plusieurs origines
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', 
+      'https://task-master-frontend-9ovp0bzkd-karimkane26s-projects.vercel.app', 
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // Méthodes HTTP autorisées
+    allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
+    credentials: true, // Accepte les cookies pour les requêtes CORS
   });
+
   app.use(cookieParser());
-  app.use(cors());
   app.useGlobalPipes(new ValidationPipe());
-    app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1'); // Préfixe global pour toutes les routes
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
