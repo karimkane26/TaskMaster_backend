6131d3669d60b9a7566a29b522df9f73c3e5d4db
Abdoul <abdoulkarim972012@gmail.com>
2025-01-16 20:34:01 +0000

    Update Database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
