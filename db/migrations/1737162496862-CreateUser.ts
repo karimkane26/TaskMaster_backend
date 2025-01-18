c997dfd4db561f398b149e4050edaf80108a1c1a
Abdoul <abdoulkarim972012@gmail.com>
2025-01-18 01:12:38 +0000

    Update Database

--- db/migrations/1737162496862-CreateUser.ts
+++ db/migrations/1737162496862-CreateUser.ts
@@ -1,19 +1,20 @@
-/* eslint-disable prettier/prettier */
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUser1734093951721 implements MigrationInterface {
-    name = 'CreateUser1734093951721'
+export class CreateUser1737162496862 implements MigrationInterface {
+    name = 'CreateUser1737162496862'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."user_entity_role_enum" AS ENUM('admin', 'user')`);
+        await queryRunner.query(`CREATE TABLE "user_entity" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" "public"."user_entity_role_enum" NOT NULL DEFAULT 'user', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "task_entity" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "description" character varying NOT NULL, "isCompleted" boolean NOT NULL DEFAULT false, "dueDate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "userId" integer, CONSTRAINT "PK_0385ca690d1697cdf7ff1ed3c2f" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "user_entity" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" "public"."user_entity_role_enum" NOT NULL DEFAULT 'user', CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "task_entity" ADD CONSTRAINT "FK_2621bebd84d2624da37a34797fc" FOREIGN KEY ("userId") REFERENCES "user_entity"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "task_entity" DROP CONSTRAINT "FK_2621bebd84d2624da37a34797fc"`);
-        await queryRunner.query(`DROP TABLE "user_entity"`);
         await queryRunner.query(`DROP TABLE "task_entity"`);
+        await queryRunner.query(`DROP TABLE "user_entity"`);
+        await queryRunner.query(`DROP TYPE "public"."user_entity_role_enum"`);
     }
 
 }
