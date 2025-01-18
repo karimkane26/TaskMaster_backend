cc0a559f1fff6b1ab5475cafb9ea77fd1211cd11
Abdoul <abdoulkarim972012@gmail.com>
2025-01-18 22:40:56 +0000

    Update Database

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,27 +1,48 @@
 /* eslint-disable prettier/prettier */
+// /* eslint-disable prettier/prettier */
 
-import { DataSource,DataSourceOptions } from "typeorm";
- import {config} from 'dotenv';
- config()
- export const dataSourceOptions:DataSourceOptions = {
-    type: 'postgres',
-       host:process.env.DB_HOST,
-       port:Number(process.env.DB_PORT),
-       username:process.env.DB_USERNAME,
-       password:process.env.DB_PASSWORD,
-       database:process.env.DB_DATABASE,
-        ssl: {
-     rejectUnauthorized: false, // Utilisez `false` pour éviter les problèmes de certificat en local
-   },
-       entities: ['dist/**/*.entity{.ts,.js}'],
-       migrations:['dist/db/migrations/*{.ts,.js}'],
-       logging:false,
-      //  synchronize:true
-      synchronize: true // Synchronisation en développement uniquement
+ import { DataSource,DataSourceOptions } from "typeorm";
+//  import {config} from 'dotenv';
+//  config()
+//  export const dataSourceOptions:DataSourceOptions = {
+//     type: 'postgres',
+//        host:process.env.DB_HOST,
+//        port:Number(process.env.DB_PORT),
+//        username:process.env.DB_USERNAME,
+//        password:process.env.DB_PASSWORD,
+//        database:process.env.DB_DATABASE,
+//         ssl: {
+//      rejectUnauthorized: false, // Utilisez `false` pour éviter les problèmes de certificat en local
+//    },
+//        entities: ['dist/**/*.entity{.ts,.js}'],
+//        migrations:['dist/db/migrations/*{.ts,.js}'],
+//        logging:false,
+//       //  synchronize:true
+//       synchronize: true // Synchronisation en développement uniquement
 
-} 
- const dataSource = new DataSource(dataSourceOptions);
+// } 
+//  const dataSource = new DataSource(dataSourceOptions);
  
- export default dataSource;
+//  export default dataSource;
 
 
+export const dataSourceOptions: DataSourceOptions = {
+   type: 'postgres',
+   host: process.env.DB_HOST,
+   port: Number(process.env.DB_PORT),
+   username: process.env.DB_USERNAME,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_DATABASE,
+   ssl: {
+     require: true, // Force l'utilisation de SSL
+     rejectUnauthorized: false, // Ignore les problèmes de certificat
+   },
+   entities: ['dist/**/*.entity{.ts,.js}'],
+   migrations: ['dist/db/migrations/*{.ts,.js}'],
+   synchronize: true, // Active uniquement en développement
+   logging: true,
+ };
+ 
+   const dataSource = new DataSource(dataSourceOptions);
+ 
+  export default dataSource;
